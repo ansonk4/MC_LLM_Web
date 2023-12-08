65f308c361aba46b9d0ec2c2b8bb86a5b37e5a08
ansonk4 <kwoktsunon@yahoo.com>
2023-12-08 16:41:07 +0800

    update main.py

--- main.py
+++ main.py
@@ -13,17 +13,16 @@ recurment.subheader(rec_tol)
 
 ending = col1.container(border = True)
 ending.write('結局')
-end = ending.radio('結局',['未完成 (+0分)', '息潮的代價 (+80分)', '命運的寵兒 (+350分)', '靜謐時代 (+350分)'],label_visibility='collapsed')
-end_to_score = {'未完成 (+0分)': 0, '命運的寵兒 (+350分)': 350, '靜謐時代 (+350分)': 350, '息潮的代價 (+80分)': 80}
-end_score = end_to_score[end]
+end1 = ending.checkbox('息潮的代價 (+80分)')
+end2 = ending.checkbox('靜謐時代 (+350分)')
+end_score = end1*80 + end2*350 
 ending.subheader(end_score)
 
 secret_floor = col1.container(border = True)
 secret_floor.write('誤入奇境')
-floor1 = secret_floor.checkbox('荒地群獵-銹錘 (+120分)')
-floor2 = secret_floor.checkbox('寒災之咒-寒災 (+150分)')
-floor3 = secret_floor.checkbox('險路勿近-墓碑 (+230分)')
-floor_score = floor1*120 + floor2*150 + floor3*230
+floor = secret_floor.radio('floor', ['未完成 (+0分)', '荒地群獵-銹錘 (+120分)', '寒災之咒-寒災 (+150分)', '險路勿近-墓碑 (+230分)'], label_visibility='collapsed')
+floor_to_score = {'未完成 (+0分)': 0, '荒地群獵-銹錘 (+120分)': 120, '寒災之咒-寒災 (+150分)': 150, '險路勿近-墓碑 (+230分)': 230}
+floor_score = floor_to_score[floor]
 secret_floor.subheader(floor_score)
 
 
@@ -33,6 +32,7 @@ e_3 = emergency.expander('第三層')
 e_3_1 = e_3.number_input('巢穴 (+40分)', step = 1)
 e_3_2 = e_3.number_input('大君遺脈 (+40分)', step = 1)
 e_3_3 = e_3.number_input('漩渦(有騎士且存活時) (+40分)', step = 1)
+e_3_4 = e_3.number_input('据險固守 (+40分)', step = 1)
 
 e_4 = emergency.expander('第四層')
 e_4_1 = e_4.number_input('海窟沙暴(無漏時) (+50分)', step = 1)
@@ -50,7 +50,7 @@ e_6 = emergency.expander('第六層')
 e_6_1 = e_6.number_input('深度認知 (+90分)', step = 1)
 e_6_2 = e_6.number_input('水火相容 (+150分)', step = 1)
 
-emergency_score = (e_3_1 + e_3_2 + e_3_3) * 40 + e_4_1 * 50 + e_4_2 * 50 + e_4_3 * 60 + e_4_4 * 100 \
+emergency_score = (e_3_1 + e_3_2 + e_3_3 + e_3_4) * 40 + e_4_1 * 50 + e_4_2 * 50 + e_4_3 * 60 + e_4_4 * 100 \
   + e_5_1 * 50 + e_5_2 * 50 + e_5_3 * 60 + e_5_4 * 60 + e_6_1 * 90 + e_6_2 * 150
 emergency.subheader(emergency_score)
 
@@ -61,8 +61,6 @@ duck_score = duck1 * 80
 duck.subheader(duck_score)
 
 
-# 【特殊挑戰】事件，
-# 真相+50分(無漏時+90分)，”喜”從箱來+50分(無漏+箱子全收時+90分)，狂信如火(無漏時)+50分。漏護盾血也算漏。
 challenge = col3.container(border = True)
 challenge.write('特殊挑戰')
 c_1 = challenge.checkbox('真相 (+50分)')
@@ -73,7 +71,6 @@ c_5 = challenge.checkbox('狂信如火(無漏時) (+50分)')
 c_total = c_1 * 50 + c_2 * 90 + c_3 * 50 + c_4 * 90 + c_5 * 50
 challenge.subheader(c_total)
 
-# 鴨爵，高普尼克，流淚小子擊殺時獲得+20分。
 
 enemy = col3.container(border = True)
 enemy.write('隱藏敵人')
@@ -82,9 +79,9 @@ en_score = en * 20
 enemy.subheader(en_score)
 
 mis = col3.container(border = True)
-Collectibles = mis.number_input('藏品 (+10分)', step = 1)
-Enlightenment = mis.number_input('啓示 (+50分)', step = 1)
-mis_score = Collectibles * 10 + Enlightenment * 50
+collectibles = mis.number_input('藏品 (+10分)', step = 1)
+enlightenment = mis.number_input('啓示 (+50分)', step = 1)
+mis_score = collectibles * 10 + enlightenment * 50
 mis.subheader(mis_score)
 
 total_score = rec_tol + end_score + floor_score + emergency_score + duck_score + c_total + en_score + mis_score
